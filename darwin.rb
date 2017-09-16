ba2765847590cec9aa601d798cf74353a56a0634
Dimitrios Douros <Dimitrios Douros>
2017-09-16 17:03:38 +0300

    2nd commit

--- darwin.rb
+++ darwin.rb
@@ -7,6 +7,7 @@ class GeneticAlgo
 		@fTotal = 0
 		@roulette = []
 		@probabilitySum = 0
+		@selectedPopulation = []
 
 		File.open("input.txt", "r") do |f|
 		  f.each_line do |line|
@@ -14,35 +15,62 @@ class GeneticAlgo
 		    #puts line
 		  end
 		end
+
+		puts "Initial Population:"
 		puts @initialPopulation
 
+	end
+
+	def run
 		@initialPopulation.each do |chromosome|
 			#puts chromosome
 			@initialPopulationFitness << chromosome.to_i(2)
 			#Calculate population fitness
 			@fTotal += chromosome.to_i(2)
 		end
+
+		puts "Initial Population Fitness:"
 		puts @initialPopulationFitness
 
+		puts "Total Fitness:" 
 		puts @fTotal 
 
 		@initialPopulationFitness.each do |chromosomeFitness|
 			@initialPopulationSelectionProbability << (Float(chromosomeFitness) / Float(@fTotal)).round(5)
 		end
 		
+		puts "Initial Population Selection Probability:"
 		puts @initialPopulationSelectionProbability
-
-		puts rand.round(5)
 
 		@initialPopulationSelectionProbability.each do |probability|
 			@probabilitySum = (@probabilitySum + probability)
 			@roulette << @probabilitySum
 		end
 
+		puts "Roulette:"
 		puts @roulette
 
+		puts ""
+
+		# Todo select atoms for reproduction
+		
+		@initialPopulation.count.times do |n|
+			chromosome = 0
+			rnd = rand.round(5); puts rnd
+			if rnd > @roulette[n] then
+				chromosome += 1
+				puts chromosome
+			else
+				@selectedPopulation << @initialPopulation[chromosome]
+			end
+			
+		end
+
+		puts "Selected Population:"
+		puts @selectedPopulation
 	end
 
 end
 
 g = GeneticAlgo.new
+g.run
